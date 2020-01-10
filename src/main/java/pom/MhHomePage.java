4b619fecd80967292e958172b790986250a03874
vinayak <vinayak@tech.internal>
2020-01-10 16:06:12 +0530

    version one,Comments by Admin a Test Sage User

---
+++ src/main/java/pom/MhHomePage.java
@@ -0,0 +1,43 @@
+
+package pom;
+
+import org.openqa.selenium.WebDriver;
+
+import org.openqa.selenium.WebElement;
+
+import org.openqa.selenium.support.FindAll;
+
+import org.openqa.selenium.support.FindBy;
+
+import org.openqa.selenium.support.PageFactory;
+
+
+
+import reuseablePackage.feature.AllActions;
+
+
+public class MhHomePage {
+
+// public WebDriver driver=null;
+// public MhHomePage (WebDriver driver)
+// 	{
+// 		this.driver = driver;
+// 		PageFactory.initElements(AllActions.driver, this);        // Reusable is a Reusable class where we initalize Web Driver ( public static WebDriver driver =null)
+//     }
+    
+ThreadLocal<WebDriver> driver;
+	 	public MhHomePage (ThreadLocal<WebDriver> driver)
+	{
+		this.driver = driver;
+		PageFactory.initElements(driver.get(), this);    
+	}	
+
+
+@FindBy(xpath="//input[@id='search']")public WebElement search2;
+
+@FindBy(xpath="//input[@id='context_path']")public WebElement context_path1;
+//pomStart
+	
+
+
+}
